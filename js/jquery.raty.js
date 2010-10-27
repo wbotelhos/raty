103edec2134ee687d1675db0cc63c886356c2144
washingtonbotelho <wbotelhos@gmail.com>
2010-10-27 15:06:43 -0200

    Reusing functions for an better code.

--- js/jquery.raty.js
+++ js/jquery.raty.js
@@ -32,7 +32,7 @@
  *		start:        0,                                               // Start with a score value.
  *		starOff:      'star-off.png',                                  // The image of the off star.
  *		starOn:       'star-on.png'                                    // The image of the on star.
- *      //onClick:    function() { alert('clicked!'); }                // Default onClick function. "this" is the star element itself.
+ *      //onClick:    function(score) { alert('score: ' + score); }    // Default onClick function. "this" is the star element itself.
  *	});
  *  
  *  <div id="star"></div>
@@ -70,55 +70,37 @@
 		$global = $(this);
 
 		// Local variables to keep the current value and not the last one. Why, Mr. Anderson? Why? 
-		var $this			= $global,
-			containerId		= $global.attr('id'),
-			path			= options.path,
-			cancelOff		= options.cancelOff,
-			cancelOn		= options.cancelOn,
-			showHalf		= options.showHalf,
-			starHalf		= options.starHalf,
-			starOff			= options.starOff,
-			starOn			= options.starOn,
-			onClick			= options.onClick,
-			start			= 0,
-			hint			= '';
+		var $this		= $global,
+			id			= $this.attr('id'),
+			start		= 0,
+			starFile	= options.starOn,
+			hint		= '';
 
 		if (!isNaN(options.start) && options.start > 0) {
 			start = (options.start > options.number) ? options.number : options.start;
 		}
 
 		for (var i = 1; i <= options.number; i++) {
+			starFile = (start >= i) ? options.starOn : options.starOff;
+
 			hint = (options.number <= options.hintList.length && options.hintList[i - 1] !== null) ? options.hintList[i - 1] : i;	// Avoids a nonexistent index (undefined) and ensures that the hint will be applied, it means different from null. Otherwise applies the current number.
 
-			starFile = (start >= i) ? starOn : starOff;
-
 			$this
-			.append('<img id="' + containerId + '-' + i + '" src="' + path + starFile + '" alt="' + i + '" title="' + hint + '" class="' + containerId + '"/>')
+			.append('<img id="' + id + '-' + i + '" src="' + options.path + starFile + '" alt="' + i + '" title="' + hint + '" class="' + id + '"/>')
 			.append((i < options.number) ? '&nbsp;' : '');
 		}
 
-		$this.append('<input id="' + containerId + '-score" type="hidden" name="' + options.scoreName + '"/>');
+		$('<input id="' + id + '-score" type="hidden" name="' + options.scoreName + '"/>').appendTo($this).val(start);
 
-		$('#' + containerId + '-score').val(start);
-
-		if (showHalf) {
-			var score = $('input#' + containerId + '-score').val(),																	// [x ... x.25] || [x.26 ... x.75] || [x.76 ... (x + 1)]
-				rounded = Math.ceil(score),																							// (x + 1)
-				diff = (rounded - score).toFixed(1);																				// [0.9 ... 0.8] || [0.7 ... 0.3] || [0.2 ... 0]
-
-			if (diff >= 0.3 && diff <= 0.7) {																						// 3.5			In the center interval the score is with a next star.
-				rounded = rounded - 0.5;																							// 3 . half
-				$('img#' + containerId + '-' + Math.ceil(rounded)).attr('src', path + starHalf);									// 				The next star is the half star.
-			} else if (diff >= 0.8) {																								// 3			In the bottom interval the score is rounded down.
-				rounded--;																											// 3 . --
-			} else {																												// 4 . --		In the top interval the score is the same rounded top.
-				$('img#' + containerId + '-' + rounded).attr('src', path + starOn);
-			}
+		if (options.showHalf) {
+			var score = $('input#' + id + '-score').val();
+			splitStar($this, score, options);
 		}
 
 		if (!options.readOnly) {
 			if (options.showCancel) {
-				var cancel = '<img src="' + path + options.cancelOff + '" alt="x" title="' + options.cancelHint + '" class="button-cancel"/>'; 
+				var star	= $('img.' + id),
+					cancel	= '<img src="' + options.path + options.cancelOff + '" alt="x" title="' + options.cancelHint + '" class="button-cancel"/>'; 
 
 				if (options.cancelPlace == 'left') {
 					$this.prepend(cancel + '&nbsp;');
@@ -126,80 +108,29 @@
 					$this.append('&nbsp;').append(cancel);
 				}
 
-				$this.css('width', options.number * 20 + 20);
-
-				$('#' + containerId + ' img.button-cancel')
-				.live('mouseenter', function() {
-					$(this).attr('src', path + cancelOn);
-					$('img.' + containerId).attr('src', path + starOff);
-				})
-				.live('mouseleave', function() {
-					$(this).attr('src', path + cancelOff);
-					$('img.' + containerId).trigger('mouseout');
-				})
-				.live('click', function() {
-					$('input#' + containerId + '-score').val(0);
-					if (onClick) { 
-			          onClick.apply($this, [0]);
+				$('#' + id + ' img.button-cancel').live('mouseenter', function() {
+					this.attr('src', options.path + options.cancelOn);
+					star.attr('src', options.path + options.starOff);
+				}).live('mouseleave', function() {
+					this.attr('src', options.path + options.cancelOff);
+					star.trigger('mouseout');
+				}).live('click', function() {
+					$('input#' + id + '-score').val(0);
+					if (options.onClick) { 
+			          options.onClick.apply($this, [0]);
 			        }
 				});
+
+				$this.css('width', options.number * 20 + 20);
 			} else {
 				$this.css('width', options.number * 20);
 			}
 
-			$('img.' + containerId)
-			.live('mouseenter', function() {
-				var qtyStar = $('img.' + containerId).length;
-
-				for (var i = 1; i <= qtyStar; i++) {
-					if (i <= this.alt) {
-						$('img#' + containerId + '-' + i).attr('src', path + starOn);
-					} else {
-						$('img#' + containerId + '-' + i).attr('src', path + starOff);
-					}
-				}
-			})
-			.live('click', function() {
-				$('input#' + containerId + '-score').val(this.alt);
-				if (onClick) { 
-		          onClick.apply($this, [this.alt]);
-		        }
-			});
-
-			$this.live('mouseleave', function() {
-				var id = $(this).attr('id'),
-					qtyStar = $('img.' + id).length,
-					score = $('input#' + id + '-score').val();
-
-				for (var i = 1; i <= qtyStar; i++) {
-					if (i <= score) {
-						$('img#' + id + '-' + i).attr('src', path + starOn);
-					} else {
-						$('img#' + id + '-' + i).attr('src', path + starOff);
-					}
-				}
-
-				if (showHalf) {
-					var score = $('input#' + id + '-score').val(),
-						rounded = Math.ceil(score),
-						diff = (rounded - score).toFixed(1);
-
-					if (diff >= 0.3 && diff <= 0.7) {
-						rounded = rounded - 0.5;
-						$('img#' + id + '-' + Math.ceil(rounded)).attr('src', path + starHalf);
-					} else if (diff >= 0.8) {
-						rounded--;
-					} else {
-						$('img#' + id + '-' + rounded).attr('src', path + starOn);
-					}
-				}
-			}).css('cursor', 'pointer');
+			$this.css('cursor', 'pointer');
+			bindAll($this, options);
 		} else {
-			hint = (options.number <= options.hintList.length && options.hintList[start - 1] !== null) ? options.hintList[start - 1] : start;
-
-			$this
-			.css('cursor', 'default').attr('title', hint)
-			.children('img').attr('title', hint);
+			$this.css('cursor', 'default');
+			fixHint($this, start, options);
 		}
 
 		return $this;
@@ -229,39 +160,41 @@
 			$('img.' + $global.attr('id')).die();
 			$global.css('cursor', 'default').die();
 		} else { 
-			liveEnter();
-			liveLeave();
-			liveClick();
+			bindAll($global, options);
 			$global.css('cursor', 'pointer');
 		}
 		return $.fn.raty;
 	};
 
-	$.fn.raty.start = function(start) {
-		initialize(start);
+	$.fn.raty.start = function(score) {
+		initialize($global, score, options);
 		return $.fn.raty;
 	};
 
 	$.fn.raty.click = function(score) {
-		var star = (score >= options.number) ? options.number : score;
+		score = (score >= options.number) ? options.number : score;
 
-		initialize(star);
+		initialize($global, score, options);
 
 		if (options.onClick) {
-			options.onClick.apply($this, [star]);
+			options.onClick.apply($global, [star]);
 		} else {
 			debug('You should add the "onClick: function() {}" option.');
 		}
 		return $.fn.raty;
 	};
-	
-	// Repeated on purpose for now! Because options.x here works as current value, unlike in the function body. Why, Mr. Anderson? Why?
-	function liveEnter() {
-		var id = $global.attr('id');
 
-		$('img.' + id).live('mouseenter', function() {
-			var qtyStar = $('img.' + id).length;
+	function bindAll(context, options) {
+		var id		= context.attr('id'),
+			score	= $('input#' + id + '-score'),
+			qtyStar	= $('img.' + id).length;
 
+		context.live('mouseleave', function() { 
+			initialize(context, score.val(), options);
+		});
+
+		$('img.' + id)
+		.live('mouseenter', function() {
 			for (var i = 1; i <= qtyStar; i++) {
 				if (i <= this.alt) {
 					$('img#' + id + '-' + i).attr('src', options.path + options.starOn);
@@ -269,40 +202,28 @@
 					$('img#' + id + '-' + i).attr('src', options.path + options.starOff);
 				}
 			}
-		});
-	};
-	
-	function liveLeave() {
-		$global.live('mouseleave', function() { 
-			var id = $(this).attr('id'),
-				qtyStar = $('img.' + id).length,
-				score = $('input#' + id + '-score').val();
-
-			for (var i = 1; i <= qtyStar; i++) {
-				if (i <= score) {
-					$('img#' + id + '-' + i).attr('src', options.path + options.starOn);
-				} else {
-					$('img#' + id + '-' + i).attr('src', options.path + options.starOff);
-				}
+		}).live('click', function() {
+			score.val(this.alt);
+			if (options.onClick) {
+				options.onClick.apply(context, [this.alt]);
 			}
 		});
 	};
 
-	function liveClick() {
-		var id = $global.attr('id');
-		$('img.' + id).live('click', function() {
-			$('input#' + id + '-score').val(this.alt);
-		});
-	};
+	function initialize(context, score, options) {
+		var id		= context.attr('id'),
+			qtyStar	= $('img.' + id).length;
 
-	function initialize(start) {
-		var id = $global.attr('id'),
-			qtyStar = $('img.' + id).length;
+		if (score < 0 || isNaN(score)) {
+			score = 0;
+		} else if (score > options.number) {
+			score = options.number;
+		}
 
-		$('input#' + id + '-score').val(start);
+		$('input#' + id + '-score').val(score);
 
 		for (var i = 1; i <= qtyStar; i++) {
-			if (i <= start) {
+			if (i <= score) {
 				$('img#' + id + '-' + i).attr('src', options.path + options.starOn);
 			} else {
 				$('img#' + id + '-' + i).attr('src', options.path + options.starOff);
@@ -310,20 +231,36 @@
 		}
 
 		if (options.showHalf) {
-			var rounded = Math.ceil(start),
-				diff = (rounded - start).toFixed(1);
-
-			if (diff >= 0.3 && diff <= 0.7) {
-				rounded = rounded - 0.5;
-				$('img#' + id + '-' + Math.ceil(rounded)).attr('src', options.path + options.starHalf);
-			} else if (diff >= 0.8) {
-				rounded--;
-			} else {
-				$('img#' + id + '-' + rounded).attr('src', options.path + options.starOn);
-			}
+			splitStar(context, score, options);
+		}
+		
+		if (options.readOnly) {
+			fixHint(context, score, options);
 		}
 	};
-	
+
+	function splitStar(context, score, options) {
+		var id		= context.attr('id'),
+			rounded	= Math.ceil(score),
+			diff	= (rounded - score).toFixed(1);
+
+		if (diff >= 0.3 && diff <= 0.7) {
+			rounded = rounded - 0.5;
+			$('img#' + id + '-' + Math.ceil(rounded)).attr('src', options.path + options.starHalf);
+		} else if (diff >= 0.8) {
+			rounded--;
+		} else {
+			$('img#' + id + '-' + rounded).attr('src', options.path + options.starOn);
+		}
+	};
+
+	function fixHint(context, score, options) {
+		score = parseInt(score);
+		hint = (score > 0 && options.number <= options.hintList.length && options.hintList[score - 1] !== null) ? options.hintList[score - 1] : score;
+
+		context.attr('title', hint).children('img').attr('title', hint);		
+	};
+
 	function debug(message) {
 		if (window.console && window.console.log) {
 			window.console.log(message);
