58b91a661272499adadcfb5956d5451451bfbbdc
Washington Botelho <wbotelhos@gmail.com>
2014-06-12 16:39:56 -0300

    refactor: extracting specs.

--- spec/lib/helper.js
+++ spec/lib/helper.js
@@ -1,28 +1,73 @@
 isClear = true;
 
 Helper = {
-  clear: function() {
-    for (var i = 0; i < this.ids.length; i++) {
-      $(this.ids[i].prefix + this.ids[i].id).remove();
-    }
+  _append: function(type, attrs) {
+    return $('<' + type + '/>', attrs).appendTo('body');
   },
 
-  create: function(id, options) {
-    var
-      attrs = options || {},
-      data  = { prefix: id.charAt(0), id: id.slice(1) };
+  _attrs: function(data, options) {
+    var attrs = options || {};
 
-    this.ids = this.ids || [];
-
-    this.ids.push(data);
-
-    if (id.charAt(0) === '#') {
+    if (data.prefix === '#') {
       attrs.id = data.id;
     } else {
       attrs['class'] = data.id;
     }
 
-    return $('<div />', attrs).appendTo('body');
+    return attrs;
+  },
+
+  _select: function() {
+    return '' +
+      '<option value="Cancel this rating!">cancel hint default</option>' +
+      '<option value="cancel-hint-custom">cancel hint custom</option>' +
+
+      '<option value="">cancel number default</option>' +
+      '<option value="0">cancel number custom</option>' +
+
+      '<option value="bad">bad hint imutable</option>' +
+      '<option value="1">bad number imutable</option>' +
+
+      '<option value="targetText">targetText is setted without targetKeep</option>' +
+
+      '<option value="gorgeous">targetFormat</option>';
+  },
+
+  _save: function(id) {
+    var data = { prefix: id.charAt(0), id: id.slice(1) };
+
+    this.ids = this.ids || [];
+
+    this.ids.push(id);
+
+    return data;
+  },
+
+  clear: function() {
+    for (var i = 0; i < this.ids.length; i++) {
+      $(this.ids[i]).remove();
+    }
+  },
+
+  create: function(id, options) {
+    var data  = this._save(id),
+        attrs = this._attrs(data, options);
+
+    return this._append('div', attrs);
+  },
+
+  target: function(id, type, options) {
+    type = type || 'div';
+
+    var data  = this._save(id),
+        attrs = this._attrs(data, options),
+        el    = '<' + type + '/>';
+
+    if (type === 'select') {
+      attrs.html = this._select();
+    }
+
+    return this._append(type, attrs);
   }
 };
 
---
+++ spec/target_spec.js
@@ -0,0 +1,247 @@
+describe('#target', function() {
+  beforeEach(function() {
+    $.fn.raty.defaults.path = '../lib/images';
+
+    this.el = Helper.create('#el');
+  });
+
+  afterEach(function() {
+    Helper.clear();
+  });
+
+  context('on mouseover', function() {
+    context('as div', function() {
+      beforeEach(function() {
+        this.target = Helper.target('#target');
+      });
+
+      it ('sets the hint', function() {
+        // given
+        this.el.raty({ target: '#' + this.target[0].id });
+
+        var star = this.el.children('img:last');
+
+        // when
+        star.trigger('mouseover');
+
+        // then
+        expect(this.target).toHaveHtml('gorgeous');
+      });
+    });
+
+    context('as input', function() {
+      beforeEach(function() {
+        this.target = Helper.target('#target', 'input');
+      });
+
+      it ('sets the hint', function() {
+        // given
+        this.el.raty({ target: '#' + this.target[0].id });
+
+        var star = this.el.children('img:last');
+
+        // when
+        star.trigger('mouseover');
+
+        // then
+        expect(this.target).toHaveValue('gorgeous');
+      });
+    });
+
+    context('as textarea', function() {
+      beforeEach(function() {
+        this.target = Helper.target('#target', 'textarea');
+      });
+
+      it ('sets the hint', function() {
+        // given
+        this.el.raty({ target: '#' + this.target[0].id });
+
+        var star = this.el.children('img:last');
+
+        // when
+        star.trigger('mouseover');
+
+        // then
+        expect(this.target).toHaveValue('gorgeous');
+      });
+    });
+
+    context('as select', function() {
+      beforeEach(function() {
+        this.target = Helper.target('#target', 'select');
+      });
+
+      it ('sets the hint', function() {
+        // given
+        this.el.raty({ target: '#' + this.target[0].id });
+
+        var star = this.el.children('img:last');
+
+        // when
+        star.trigger('mouseover');
+
+        // then
+        expect(this.target).toHaveValue('gorgeous');
+      });
+    });
+
+    context('and mouseout', function() {
+      context('as div', function() {
+        beforeEach(function() {
+          this.target = Helper.target('#target');
+        });
+
+        it ('gets clear', function() {
+          // given
+          this.el.raty({ target: '#' + this.target[0].id });
+
+          var star = this.el.children('img:last');
+
+          // when
+          star.trigger('mouseover').trigger('mouseout');
+
+          // then
+          expect(this.target).toBeEmpty();
+        });
+      });
+
+      context('as textarea', function() {
+        beforeEach(function() {
+          this.target = Helper.target('#textarea');
+        });
+
+        it ('gets clear', function() {
+          // given
+          this.el.raty({ target: '#' + this.target[0].id });
+
+          var star = this.el.children('img:last');
+
+          // when
+          star.trigger('mouseover').trigger('mouseout');
+
+          // then
+          expect(this.target).toHaveValue('');
+        });
+      });
+
+      context('as input', function() {
+        beforeEach(function() {
+          this.target = Helper.target('#target', 'input');
+        });
+
+        it ('gets clear', function() {
+          // given
+          this.el.raty({ target: '#' + this.target[0].id });
+
+          var star = this.el.children('img:last');
+
+          // when
+          star.trigger('mouseover').trigger('mouseout');
+
+          // then
+          expect(this.target).toHaveValue('');
+        });
+      });
+
+      context('as select', function() {
+        beforeEach(function() {
+          this.target = Helper.target('#select');
+        });
+
+        it ('gets clear', function() {
+          // given
+          this.el.raty({ target: '#' + this.target[0].id });
+
+          var star = this.el.children('img:last');
+
+          // when
+          star.trigger('mouseover').trigger('mouseout');
+
+          // then
+          expect(this.target).toHaveValue('');
+        });
+      });
+    });
+
+    context('and click', function() {
+      context('and mouseout', function() {
+        context('as div', function() {
+          beforeEach(function() {
+            this.target = Helper.target('#target');
+          });
+
+          it ('gets clear', function() {
+            // given
+            this.el.raty({ target: '#' + this.target[0].id });
+
+            var star = this.el.children('img:last');
+
+            // when
+            star.trigger('mouseover').trigger('click').trigger('mouseout');
+
+            // then
+            expect(this.target).toBeEmpty();
+          });
+        });
+
+        context('as textarea', function() {
+          beforeEach(function() {
+            this.target = Helper.target('#textarea');
+          });
+
+          it ('gets clear', function() {
+            // given
+            this.el.raty({ target: '#' + this.target[0].id });
+
+            var star = this.el.children('img:last');
+
+            // when
+            star.trigger('mouseover').trigger('click').trigger('mouseout');
+
+            // then
+            expect(this.target).toHaveValue('');
+          });
+        });
+
+        context('as input', function() {
+          beforeEach(function() {
+            this.target = Helper.target('#target', 'input');
+          });
+
+          it ('gets clear', function() {
+            // given
+            this.el.raty({ target: '#' + this.target[0].id });
+
+            var star = this.el.children('img:last');
+
+            // when
+            star.trigger('mouseover').trigger('click').trigger('mouseout');
+
+            // then
+            expect(this.target).toHaveValue('');
+          });
+        });
+
+        context('as select', function() {
+          beforeEach(function() {
+            this.target = Helper.target('#select');
+          });
+
+          it ('gets clear', function() {
+            // given
+            this.el.raty({ target: '#' + this.target[0].id });
+
+            var star = this.el.children('img:last');
+
+            // when
+            star.trigger('mouseover').trigger('click').trigger('mouseout');
+
+            // then
+            expect(this.target).toHaveValue('');
+          });
+        });
+      });
+    });
+  });
+});
