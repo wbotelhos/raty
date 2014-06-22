6aa94752e3d74aa01c1a2b02306e3b9b8fc0f47a
Washington Botelho <wbotelhos@gmail.com>
2014-06-22 02:34:59 -0300

    remove: default values. add: lib css.

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -1,16 +1,12 @@
 module.exports = function(config) {
   config.set({
-    autoWatch      : true,
-    basePath       : '',
-    browsers       : ['Firefox'],
-    captureTimeout : 60000,
-    colors         : true,
-    exclude        : [],
-    files          : ['../vendor/*.js', '../lib/*.js', 'lib/jasmine-jquery.js', 'lib/helper.js', 'options_spec.js', '*spec.js' ],
-    frameworks     : ['jasmine'],
-    logLevel       : config.LOG_INFO,
-    port           : 9876,
-    reporters      : ['progress'],
-    singleRun      : true
+    autoWatch  : true,
+    browsers   : ['Firefox'],
+    files      : ['../vendor/*.js', '../lib/*.css', '../lib/*.js', 'lib/jasmine-jquery.js', 'lib/helper.js', 'options_spec.js', '*spec.js' ],
+    frameworks : ['jasmine'],
+    logLevel   : config.LOG_ERROR,
+    port       : 9876,
+    reporters  : ['dots'],
+    singleRun  : true
   });
 };
