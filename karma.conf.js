f6b00ee897f8e61902bc66863cc8d2b1740162d5
Washington Botelho <wbotelhos@gmail.com>
2020-10-18 16:15:07 -0300

    ref: moves jasmine jquery to vendor folder

--- karma.conf.js
+++ karma.conf.js
@@ -9,9 +9,8 @@ module.exports = function(config) {
       'vendor/*.js',
       'lib/*.css',
       'lib/*.js',
-      'spec/lib/jasmine-jquery.js',
+      'spec/vendor/jasmine-jquery.js',
       'spec/lib/helper.js',
-      'spec/options_spec.js',
       'spec/*spec.js'
     ],
 
