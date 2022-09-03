4462a886ac724195585f7e1da9017fc803b21454
Washington Botelho <wbotelhos@gmail.com>
2022-09-03 11:04:59 -0300

    up: moves helper to the spec root path

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,21 @@
-module.exports = function(config) {
+module.exports = function (config) {
   'use strict';
 
   config.set({
     autoWatch: true,
-    browsers:  ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox'],
 
     files: [
       'node_modules/jquery/dist/jquery.min.js',
       'lib/*.css',
       'lib/*.js',
       'spec/vendor/jasmine-jquery.js',
-      'spec/lib/helper.js',
-      'spec/**/*[sS]pec.js'
+      'spec/helper.js',
+      'spec/**/*[sS]pec.js',
     ],
 
     frameworks: ['jasmine'],
-    logLevel:   config.LOG_DISABLE,
-    singleRun:  true
+    logLevel: config.LOG_DISABLE,
+    singleRun: true,
   });
 };
--- spec/helper.js
+++ spec/helper.js
@@ -1,17 +1,19 @@
-function context(description, spec) { // eslint-disable-line no-redeclare, no-unused-vars
+function context(description, spec) {
+  // eslint-disable-line no-redeclare, no-unused-vars
   describe(description, spec);
 }
 
-afterEach(function() {
+afterEach(function () {
   $.raty.path = undefined;
 });
 
-var Helper = { // eslint-disable-line no-redeclare, no-unused-vars
-  _append: function(type, attrs) {
+var Helper = {
+  // eslint-disable-line no-redeclare, no-unused-vars
+  _append: function (type, attrs) {
     return $('<' + type + '/>', attrs).appendTo('body');
   },
 
-  _attrs: function(data, options) {
+  _attrs: function (data, options) {
     var attrs = options || {};
 
     if (data.prefix === '#') {
@@ -23,23 +25,21 @@ var Helper = { // eslint-disable-line no-redeclare, no-unused-vars
     return attrs;
   },
 
-  _select: function() {
-    return '' +
+  _select: function () {
+    return (
+      '' +
       '<option value="Cancel this rating!">cancel hint default</option>' +
       '<option value="cancel-hint-custom">cancel hint custom</option>' +
-
       '<option value="">cancel number default</option>' +
       '<option value="0">cancel number custom</option>' +
-
       '<option value="bad">bad hint imutable</option>' +
       '<option value="1">bad number imutable</option>' +
-
       '<option value="targetText">targetText is setted without targetKeep</option>' +
-
-      '<option value="gorgeous">targetFormat</option>';
+      '<option value="gorgeous">targetFormat</option>'
+    );
   },
 
-  _save: function(id) {
+  _save: function (id) {
     var data = { prefix: id.charAt(0), id: id.slice(1) };
 
     this.ids = this.ids || [];
@@ -49,7 +49,7 @@ var Helper = { // eslint-disable-line no-redeclare, no-unused-vars
     return data;
   },
 
-  clear: function() {
+  clear: function () {
     if (this.ids) {
       for (var i = 0; i < this.ids.length; i++) {
         $(this.ids[i]).remove();
@@ -57,27 +57,27 @@ var Helper = { // eslint-disable-line no-redeclare, no-unused-vars
     }
   },
 
-  create: function(id, type, options) {
+  create: function (id, type, options) {
     type = type || 'div';
 
-    var data  = this._save(id);
+    var data = this._save(id);
     var attrs = this._attrs(data, options);
 
     return this._append(type, attrs);
   },
 
-  click: function(el, integer, decimal) {
+  click: function (el, integer, decimal) {
     this.mouseTrigger('mousemove', el, integer, decimal);
     this.mouseTrigger('click', el, integer, decimal);
   },
 
-  mouseData: function(el, integer, decimal) {
-    var stars    = el.children('img:not(.raty-cancel)');
-    var star     = stars.eq(integer);
-    var width    = star[0].width || parseFloat(star.css('font-size'));
+  mouseData: function (el, integer, decimal) {
+    var stars = el.children('img:not(.raty-cancel)');
+    var star = stars.eq(integer);
+    var width = star[0].width || parseFloat(star.css('font-size'));
     var fraction = width / 10;
-    var left     = star.offset().left;
-    var pageX    = left + fraction * decimal + 0.1;
+    var left = star.offset().left;
+    var pageX = left + fraction * decimal + 0.1;
 
     // if (console && console.log) {
     //   console.debug(integer + '.' + decimal, ':', 'left:', left, 'width:', width, 'fraction (width/10):', fraction, 'pageX:', pageX, 'fractions (decimal * fraction)', decimal * fraction);
@@ -86,22 +86,21 @@ var Helper = { // eslint-disable-line no-redeclare, no-unused-vars
     return { el: star, pageX: pageX };
   },
 
-  mouseTrigger: function(action, el, integer, decimal) {
-    var
-      data  = this.mouseData(el, integer, decimal);
-    var evt   = $.Event(action, { pageX: data.pageX });
+  mouseTrigger: function (action, el, integer, decimal) {
+    var data = this.mouseData(el, integer, decimal);
+    var evt = $.Event(action, { pageX: data.pageX });
 
     data.el.trigger(evt);
   },
 
-  mousemove: function(el, integer, decimal) {
+  mousemove: function (el, integer, decimal) {
     this.mouseTrigger('mousemove', el, integer, decimal);
   },
 
-  target: function(id, type, options) {
+  target: function (id, type, options) {
     type = type || 'div';
 
-    var data  = this._save(id);
+    var data = this._save(id);
     var attrs = this._attrs(data, options);
 
     if (type === 'select') {
@@ -109,5 +108,5 @@ var Helper = { // eslint-disable-line no-redeclare, no-unused-vars
     }
 
     return this._append(type, attrs);
-  }
+  },
 };
