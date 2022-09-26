b6dbbbd527f4f71e201f58ad99c042684967199e
Washington Botelho <wbotelhos@gmail.com>
2022-09-26 09:45:18 -0300

    up: adds non minify version for test

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+const babel = require('gulp-babel');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const source = gulp.src('./lib/js/Raty.js');
+const uglifyJS = require('gulp-uglify');
+
+gulp.task('amd', async () => {
+  source
+    .pipe(babel({ plugins: ['@babel/plugin-transform-modules-amd'] }))
+    .pipe(uglifyJS())
+    .pipe(rename('raty.amd.min.js'))
+    .pipe(gulp.dest('./lib/js/build/', { overwrite: true }));
+});
+
+gulp.task('umd', async () => {
+  source
+    .pipe(babel({ plugins: ['@babel/plugin-transform-modules-umd'] }))
+    .pipe(uglifyJS())
+    .pipe(rename('raty.umd.min.js'))
+    .pipe(gulp.dest('./lib/js/build/', { overwrite: true }));
+});
+
+gulp.task('commonjs', async () => {
+  source
+    .pipe(babel({ plugins: ['@babel/plugin-transform-modules-commonjs'] }))
+    .pipe(uglifyJS())
+    .pipe(rename('raty.commonjs.min.js'))
+    .pipe(gulp.dest('./lib/js/build/', { overwrite: true }));
+});
+
+gulp.task('systemjs', async () => {
+  source
+    .pipe(babel({ plugins: ['@babel/plugin-transform-modules-systemjs'] }))
+    .pipe(uglifyJS())
+    .pipe(rename('raty.systemjs.min.js'))
+    .pipe(gulp.dest('./lib/js/build/', { overwrite: true }));
+});
+
+gulp.task('es6', async () => {
+  source
+    .pipe(babel())
+    .pipe(uglifyJS())
+    .pipe(rename('raty.module.min.js'))
+    .pipe(gulp.dest('./lib/js/build/', { overwrite: true }));
+});
+
+gulp.task('es5', async () => {
+  source
+    .pipe(babel({ plugins: ['babel-plugin-remove-import-export'] }))
+    .pipe(uglifyJS())
+    .pipe(rename('raty.min.js'))
+    .pipe(gulp.dest('./lib/js/build/', { overwrite: true }));
+});
+
+gulp.task('es5-test', async () => {
+  source
+    .pipe(babel({ plugins: ['babel-plugin-remove-import-export'] }))
+    .pipe(rename('raty.js'))
+    .pipe(gulp.dest('./lib/js/build/', { overwrite: true }));
+});
+
+const tasks = gulp.parallel([
+  'amd',
+  'umd',
+  'commonjs',
+  'systemjs',
+  'es6',
+  'es5',
+  'es5-test',
+]);
+
+gulp.task('build', tasks);
+
+gulp.task('watch', async () => {
+  gulp.watch(['lib/js/*.js', '!lib/js/build/*.js'], tasks);
+});
